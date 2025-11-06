9c95ee63e346bb0df27120e075078a70c73269f6
Gabriel Santos da Silva <leirbag2212@gmail.com>
2025-11-05 22:51:46 -0300

    "Novos datasets e novas alterações no código"

--- analise_datasets.py
+++ analise_datasets.py
@@ -1,15 +1,35 @@
 import pandas as pd
+import os
 
-# # df = pd.read_parquet('datasets - parquet\datasets - Balanco_Dessem_Detalhe-2025-05-23.parquet')
+# df1 = pd.read_csv('datasets - parquet\consumo_energia_nordeste.csv')
+# df1.to_parquet('seu_arquivo.parquet', engine='pyarrow')
 
-# # print(df[df['cod_subsistema'] == "NE"].head())
 
-lista_path = ['datasets - parquet\datasets para conversão\SETOR_INDUSTRIAL_POR_RG.csv','datasets - parquet\datasets para conversão\SETOR_INDUSTRIAL_POR_UF.csv']
+# print(df_test.dtypes)
+# print(df_test.head(15))
 
-for path in lista_path:
+# Caminho dos datasets 
+pathData = 'datasets - parquet'
+pathData_processado = 'DB'
 
-    df = pd.read_csv(f'{path}')
+# Listagens dos datasets
+listaDatasets = os.listdir(pathData)
+Datasets_processados = os.listdir(pathData_processado)
 
-    newPath = path.replace('.csv','.parquet')
+# processando = []
 
-    df.to_parquet(f'{newPath}')
+# for data in listaDatasets:
+#     if data.startswith('datasets - Balanco'):
+#         processando.append(data)
+#         df = pd.read_parquet(f'{data}')
+
+df = pd.read_parquet('datasets - parquet\datasets - Balanco_Dessem_Detalhe-2025-05-23.parquet')
+
+df['val_ger_fotovoltaica'] = pd.to_numeric(df['val_ger_fotovoltaica'], errors='coerce')
+df['val_ger_mmgd'] = pd.to_numeric(df['val_ger_mmgd'], errors='coerce')
+df['val_cons_elevatoria'] = pd.to_numeric(df['val_cons_elevatoria'], errors='coerce')
+
+df.drop(labels='val_ger_fotovoltaica', axis=1)
+print(df.head())
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-io.open('datasets - parquet\datasets para conversão')
+import pandas as pd
+import os
+
+df1 = pd.read_csv('datasets - parquet\consumo_energia_nordeste.csv')
+df1.to_parquet('seu_arquivo.parquet', engine='pyarrow')
